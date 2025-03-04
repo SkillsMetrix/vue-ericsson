b3dab6160efb51c2e9db732dead7d2610bf6875e
skillsmetrix Technologies <161203473+SkillsMetrix@users.noreply.github.com>
2025-03-04 13:46:20 +0530

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="container">
+    <Header />
+    <hr />
+    <div class="row">
+      <Server />
+      <ServerDetails />
+    </div>
+    <hr />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import MainApp from "./components/MainApp.vue";
+import Server from "./components/server/Server.vue";
+import Header from "./components/shared/Header.vue";
+import Footer from "./components/shared/Footer.vue";
+import ServerDetails from "./components/server/ServerDetails.vue";
+export default {
+  components: {
+    MainApp,
+    Server,
+    Header,
+    Footer,
+    ServerDetails,
+  },
+};
+</script>
+
+<style>
+p {
+  text-align: center;
+}
+</style>
